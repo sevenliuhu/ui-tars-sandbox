d6016d1e2dc193d468dd73a93d61f14326a38f99
HOMO_LAU <16208204@qq.com>
2026-05-21 13:34:47 +0800

    add license validator

--- license-validator.js
+++ license-validator.js
@@ -1,2 +1,7 @@
-/* Copyright (C) 2026 HOMO AI. License required. */
-const _k=process.env.HOMO_LICENSE_KEY;const licenseOk=_k&&_k.length>=16;if(!licenseOk)console.warn('[HOMO] License required. Contact: 16208204@qq.com');module.exports={validateLicense:()=>licenseOk};
+/* Copyright (c) 2026 HOMO AI. Proprietary. License required. Contact: 16208204@qq.com */
+
+const _key = process.env.HOMO_LICENSE_KEY;
+if (!_key || _key.length < 16) {
+    console.warn('[HOMO] License required. Commercial use needs a valid key. Contact: 16208204@qq.com');
+}
+module.exports = { validateLicense: () => _key && _key.length >= 16 };
